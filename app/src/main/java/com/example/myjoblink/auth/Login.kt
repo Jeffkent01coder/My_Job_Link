72e5b92c607c4992e54e248c860b304b8c31eed2
jeff <geoffreyerastus956@gmail.com>
2024-02-01 23:00:49 -0500

    firebase login page

--- app/src/main/java/com/example/myjoblink/auth/Login.kt
+++ app/src/main/java/com/example/myjoblink/auth/Login.kt
@@ -1,12 +1,74 @@
 package com.example.myjoblink.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.myjoblink.R
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myjoblink.dashboard.Home
+import com.example.myjoblink.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            registerEvents()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
+        }
+
+    }
+
+    private fun registerEvents() {
+        auth = FirebaseAuth.getInstance()
+        binding.btnLogin.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passEt.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                binding.progressBar.visibility = View.VISIBLE
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(
+                    OnCompleteListener {
+                        if (it.isSuccessful) {
+                            binding.emailEt.text?.clear()
+                            binding.passEt.text?.clear()
+                            Toast.makeText(
+                                this,
+                                "Logged In successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        } else {
+                            Toast.makeText(this, it.exception!!.message, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        if (email.isNotEmpty() && pass.isNotEmpty()) {
+                            startActivity(Intent(this, Home::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        binding.progressBar.visibility = View.GONE
+                    })
+            } else {
+                Toast.makeText(
+                    this,
+                    "Empty Fields Not Allowed",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
