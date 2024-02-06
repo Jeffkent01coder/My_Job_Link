8b27f4e2e30907557183900ac9385641bf8bb858
jeff <geoffreyerastus956@gmail.com>
2024-02-05 20:27:39 -0500

    job adapter

---
+++ app/src/main/java/com/example/myjoblink/jobadapter/JobAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.myjoblink.jobadapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myjoblink.databinding.JobItemBinding
+import com.example.myjoblink.model.JobData
+
+class JobAdapter(private val list: ArrayList<JobData>, val clickListener: onJobClickListener) :
+    RecyclerView.Adapter<JobAdapter.JobViewHolder>() {
+
+    inner class JobViewHolder(val jobItemBinding: JobItemBinding) :
+        RecyclerView.ViewHolder(jobItemBinding.root) {
+            fun setData(job: JobData, action: onJobClickListener){
+                jobItemBinding.apply {
+                    jobTitle.text = job.jobTitle
+                    jobLocation.text = job.jobLocation
+                    jobNature.text = job.jobNature
+                    jobPay.text = job.jobPay
+                }
+                jobItemBinding.root.setOnClickListener {
+                    action.onJObClick(job, adapterPosition)
+                }
+            }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
+        return JobViewHolder(
+            JobItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
+        val job = list[position]
+        holder.setData(job, clickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    interface onJobClickListener {
+        fun onJObClick(job: JobData, position: Int)
+    }
+
+}
---
+++ app/src/main/java/com/example/myjoblink/model/JobData.kt
@@ -0,0 +1,12 @@
+package com.example.myjoblink.model
+
+data class JobData(
+    val id: String? = "",
+    val jobUrl : String? = "",
+    val jobTitle : String? = "",
+    val jobLocation : String? = "",
+    val jobNature : String? = "",
+    val jobPay : String? = "",
+    val jobDescription : String? = ""
+
+)
