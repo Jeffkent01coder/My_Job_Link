44ab6fb00499e20822d3b0eed550517095b77624
jeff <geoffreyerastus956@gmail.com>
2024-02-09 16:16:15 -0500

    job application

---
+++ app/src/main/java/com/example/myjoblink/job/JobApplication.kt
@@ -0,0 +1,12 @@
+package com.example.myjoblink.job
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myjoblink.R
+
+class JobApplication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_application)
+    }
+}
--- app/src/main/java/com/example/myjoblink/job/JobDescription.kt
+++ app/src/main/java/com/example/myjoblink/job/JobDescription.kt
@@ -1,15 +1,13 @@
 package com.example.myjoblink.job
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.myjoblink.R
 import com.example.myjoblink.databinding.ActivityJobDescriptionBinding
-import java.net.URI
 
 class JobDescription : AppCompatActivity() {
-    private lateinit var binding : ActivityJobDescriptionBinding
+    private lateinit var binding: ActivityJobDescriptionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityJobDescriptionBinding.inflate(layoutInflater)
         supportActionBar?.hide()
@@ -25,10 +23,14 @@ class JobDescription : AppCompatActivity() {
         binding.apply {
             intent
             jobTitle.text = intent.getStringExtra("title")
-             jobLocation.text = intent.getStringExtra("location")
-             jobNature.text = intent.getStringExtra("nature")
-             jobPay.text = intent.getStringExtra("pay")
+            jobLocation.text = intent.getStringExtra("location")
+            jobNature.text = intent.getStringExtra("nature")
+            jobPay.text = intent.getStringExtra("pay")
             jobDescription.text = intent.getStringExtra("description")
+        }
+
+        binding.btnApplyJob.setOnClickListener {
+            startActivity(Intent(this, JobApplication::class.java))
         }
 
 
